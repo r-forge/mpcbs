0cbc6126ff95ae7c0ad10ab34bf534b6f14cf071
nrzhang (Professor ) <nzhang@stanford.edu>
2009-08-17 19:50:33 +0000

    Rewrote Rd files for mpcbs, mpscan.  Put scatter.ci and scatter.cpfit.R into internal.r.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpcbs@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mpcbs/R/internal.r
+++ pkg/mpcbs/R/internal.r
@@ -1280,6 +1280,41 @@ function(x, w, alpha.init =NULL,  r.init = NULL, max.iter=100, epsilon=0.01, x.l
 
 
 
+`scatter.ci` <-
+function(x,y,x.ci, y.ci, line.col="darkgray", col="black", ...){
+    plot(x, y, col=col,...)
+    for(i in 1:length(x)){
+        segments(x[i]-x.ci[i], y[i], x[i]+x.ci[i], y[i], col=line.col)
+        segments(x[i], y[i]-y.ci[i],  x[i], y[i]+y.ci[i], col=line.col)
+    }
+    points(x, y, col=col,...)
+}
+
+
+
+
+
+
+`scatter.cpfit` <-
+function(x,w,x.hat,r,alpha, platform.names){
+  K=ncol(x)
+  par(mfrow=c(K-1,K-1))
+        for(i in 1:(K-1)){
+            for(j in (i+1):K){
+               scatter.ci(x[,i], x[,j], 2*sd[,i], 2*sd[,j], cex=1.5, xlab=platform.names[[i]], ylab=platform.names[[j]], cex.lab=1.5)
+               abline(alpha[j]-alpha[i]*r[j]/r[i], r[j]/r[i])
+               
+               points(x.hat[,i], x.hat[,j], col="red")
+             #  for(a in 1:n){
+             #   segments(x[a,i],x[a,j], x.hat[a,i], x.hat[a,j], col="red")
+             #  }
+            }
+        }    
+}
+
+
+
+
 
 
 
